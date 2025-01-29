ce85b7c0873d5bffe14fe0f707f82bb474937c62
Marco <marco1arcos@gmail.com>
2025-01-29 19:52:03 +0100

    Responsive y hover

--- Tema 7 - React/Practicas/Practica1/practicaLibros/src/components/Estructura/Navegacion.jsx
+++ Tema 7 - React/Practicas/Practica1/practicaLibros/src/components/Estructura/Navegacion.jsx
@@ -7,9 +7,9 @@ const Navegacion =()=>{
         <Fragment>
             <nav className="menu">
                 <ul className="menu__lista">
-                    <li className="menu__item">Inicio</li>
-                    <li className="menu__item">Crear Libros</li>
-                    <li className="menu__item">Buscar Libros</li>
+                    <li className="menu__item" ><a className="menu__link" href="#">Inicio</a></li>
+                    <li className="menu__item"> <a  className="menu__link" href="#">Crear Libros</a></li>
+                    <li className="menu__item"><a  className="menu__link" href="#">Buscar Libros</a></li>
                 </ul>
             </nav>
         </Fragment>
